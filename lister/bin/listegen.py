089799cea3a63448cd7392174b7460fb1e9a768f
brinchj <brinchj@gmail.com>
2010-04-11 18:30:56 +0100

    minor cleanup

--- lister/bin/listegen.py
+++ lister/bin/listegen.py
@@ -7,56 +7,53 @@
 # 2010.
 
 from __future__ import with_statement
-
-import sys
-import os
-import re
+import sys, os, re
 
 if len(sys.argv) != 4:
     print "Requires exactly three parameters: paper size, column description file, and first column data"
     exit(os.EX_USAGE)
 
+(_, paper_size, desc_file, data_file) = sys.argv
+
 widths = []
 headers = []
 
-with open(sys.argv[2], 'r') as f:
-    for line in f:
-        match = re.match("^([^ ]*) (.*)$", line)
-        widths.append("p{" + match.group(1) + "}")
-        headers.append("\\vspace{0.2cm}" + match.group(2))
+for line in open(desc_file, 'r'):
+    match = re.match("^([^ ]*) (.*)$", line)
+    widths.append("p{%s}" % match.group(1))
+    headers.append(r"\vspace{0.2cm}" + match.group(2))
 
 russes = []
 
-with open(sys.argv[3], 'r') as f:
-    for line in f:
-        empties = "\\\\\\hline"
-        for i in range(1,len(headers)):
-            empties = "&" + empties
-        russes.append(line + "\\vspace{0.2cm}" + empties)
+for line in open(data_file):
+    empties = r"\\\hline"
+    for i in range(1,len(headers)):
+        empties = "&" + empties
+    russes.append(line + r"\vspace{0.2cm}" + empties)
 
-meat = """\\begin{center}
-\\begin{longtable}{%(widths)s}\\hline
-%(headers)s\\endhead
-\\hline
+meat = r"""\begin{center}
+\begin{longtable}{%(widths)s}\hline
+%(headers)s\endhead
+\hline
 %(russes)s
-\\end{longtable}
-\\end{center}""" % {'papersize' : sys.argv[1],
-                    'widths': "|" + "|".join(widths) + "|",
-                    'headers': " & ".join(headers),
-                    'russes': "\n".join(russes) }
+\end{longtable}
+\end{center}""" % {'papersize' : paper_size,
+                   'widths': "|%s|" % ("|".join(widths)),
+                   'headers': " & ".join(headers),
+                   'russes': "\n".join(russes) }
 
-if sys.argv[1] == "a3":
-    meat = "\\begin{landscape}\n" + meat + "\\end{landscape}\n"
+if paper_size == "a3":
+    meat = "\\begin{landscape}\n%s\\end{landscape}\n" % meat
 
-print """\\documentclass[11pt]{article}
-\\usepackage[utf8]{inputenc}
-\\usepackage{%(papersize)s,lscape}
-\\usepackage[danish]{babel}
-\\usepackage[T1]{fontenc}
-\\usepackage{longtable}
-\\usepackage[top=1cm, bottom=1cm, left=1cm, right=1cm]{geometry}
-\\pagestyle{empty}
-\\begin{document}
+print r"""\documentclass[11pt]{article}
+\usepackage[utf8]{inputenc}
+\usepackage{%(papersize)s,lscape}
+\usepackage[danish]{babel}
+\usepackage[T1]{fontenc}
+\usepackage{longtable}
+\usepackage[top=1cm, bottom=1cm, left=1cm, right=1cm]{geometry}
+\pagestyle{empty}
+\begin{document}
 %(meat)s
-\\end{document}
-""" % { 'papersize': sys.argv[1], 'meat': meat }
+\end{document}
+""" % { 'papersize': paper_size, 'meat': meat }
