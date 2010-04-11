6042799bca4a04a8db2cefd0304e8147a17d0522
brinchj <brinchj@gmail.com>
2010-04-11 23:12:55 +0100

    Add myself to contributors.

---
+++ lister/templates/auto/list.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook "list"
+ (lambda ()
+    (TeX-run-style-hooks
+     "xcolor"
+     "table"
+     "color"
+     "usenames"
+     "dvipsnames"
+     "geometry"
+     "longtable"
+     "fontenc"
+     "T1"
+     "babel"
+     "danish"
+     "{{papersize"
+     ""
+     "inputenc"
+     "utf8"
+     "latex2e"
+     "art11"
+     "article"
+     "11pt")))
+
