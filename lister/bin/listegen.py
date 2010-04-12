14178fca6b9634ec1775f699104bd3ef50fbe550
brinchj <brinchj@gmail.com>
2010-04-12 15:07:41 +0100

    Add supervisors (vejledere)

--- lister/bin/listegen.py
+++ lister/bin/listegen.py
@@ -12,11 +12,11 @@ sys.path += [sys.path[0] + '/..']
 from lib import tempita
 
 
-if len(sys.argv) != 4:
-    print "Requires exactly three parameters: paper size, column description file, and first column data"
+if len(sys.argv) != 5:
+    print "Requires exactly four parameters: paper size, column description file, russes file and supervisors file"
     exit(os.EX_USAGE)
 
-papersize, desc_file, data_file = sys.argv[1:]
+papersize, desc_file, rus_file, sup_file = sys.argv[1:]
 
 widths = []
 headers = []
@@ -26,13 +26,15 @@ for line in open(desc_file, 'r'):
     widths.append(match.group(1))
     headers.append(match.group(2))
 
-cap = lambda names: ' '.join(name.capitalize() for name in names.split())
-russes = sorted(map(cap, open(data_file)))
+prep = lambda names: ' '.join(name.capitalize() for name in names.split())
+read = lambda path: sorted(map(prep, open(path)))
+russes = read(rus_file)
+supers = read(sup_file)
 empties = '& '*(len(headers)-1)
 title = os.path.basename(desc_file).rsplit('.',1)[0]
 
 templ = file('templates/list.tex').read()
 print tempita.sub(templ, papersize=papersize, widths=widths,
-                  headers=headers, russes=russes, title=title,
-                  empties=empties)
+                  headers=headers, russes=russes, supers=supers,
+                  title=title,empties=empties)
 
