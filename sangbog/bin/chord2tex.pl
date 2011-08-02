02ec769ecc41b24c17164d2951503c962468e600
Troels Henriksen <athas@sigkill.dk>
2011-08-02 12:32:19 +0200

    \Needspace i stedet for \needspace.

--- sangbog/bin/chord2tex.pl
+++ sangbog/bin/chord2tex.pl
@@ -40,8 +40,7 @@ sub doit ($)
         print "\\end\{comment\}\n";
 	$ignoreblank = 1;
     } elsif ($line =~ m/^\{t:([^}]*)\}/) { # {t:
-        #print "\\needspace{6cm}";
-		print "\\newpage";
+        print "\\Needspace{6cm}";
 	print "\\begin{center}\n";
         print "\\section\*\{$1\} \n";
 	print STDERR "++$line\n";
